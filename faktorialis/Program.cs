82e49ff623743f76c1412a3162a2d1378bf483d8
knnorbi <kluka.norbert@gmail.com>
2018-09-25 14:26:58 +0200

    Initial commit

---
+++ faktorialis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktorialis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot!");
+            int n = int.Parse(Console.ReadLine());
+            
+            int faktor = 1;
+            for (int i = 1; i <= n; i++)
+                faktor *= i;
+
+            Console.WriteLine(n + "!=" + faktor);
+
+            Console.ReadKey();
+        }
+    }
+}
